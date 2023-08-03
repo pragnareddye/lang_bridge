40ead38cded6972b20a1d44a372c85ccd99c85ba
pragnareddye <pragnareddye@gmail.com>
2023-08-02 17:13:04 -0700

    Fix Docs Landing Page <head>

--- src/docs_html_generator/build.sh
+++ src/docs_html_generator/build.sh
@@ -12,8 +12,11 @@ do
     | sed -e 's,<body>,<body onload="hljs_language_augment();hljs.highlightAll();">,' > build/www/$destination
 
 done
+default_language=kannada
 cat README.md | sed 's,docs\/\([a-z]*\)\.md,\1,g' > build/README.md
 ./node_modules/.bin/markdown build/README.md --title="Lang Bridge" \
   | sed -e 's,https\:\/\/sai.onl\/lang_bridge\/,,g' \
-  | sed -e '/<title>/r src/docs_html_generator/post_title.xml' > build/www/index.html
+  | sed -e '/<title>/r src/docs_html_generator/post_title.xml' \
+  | sed -e "s,\${language},$default_language,g" \
+  | sed -e 's,<body>,<body onload="hljs_language_augment();hljs.highlightAll();">,' > build/www/index.html
 ./src/docs_html_generator/build.js
